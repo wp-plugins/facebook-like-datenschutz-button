c1e9d50453edaaa52a8f2151281daa789d9e8e75
fanpagecreator <fanpagecreator@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-05 14:49:40 +0000

    Version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/facebook-like-datenschutz-button/trunk@433587 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- facebook-like-button.php
+++ facebook-like-button.php
@@ -3,24 +3,112 @@
 Plugin Name: Facebook Like Datenschutz Button 
 Plugin URI: http://www.fanpagecreator.net/facebook-like-datenschutz-plugin/
 Description: Facebook Like Button datenschutzkonform einsetzen
-Version: 1.00
+Version: 1.2
 Author: David Seffer
 Author URI: http://davidseffer.com
 */
 
+class FacebookLike extends WP_Widget {
+	function FacebookLike() {
+		parent::WP_Widget(false, $name = 'Facebook Like Box');
+	}
+	
+	function get_post_permalink(){
+		if(is_home()){
+			$args = array( 'numberposts' => 1 );
+			$posts = get_posts( $args );
+			return get_permalink($posts[0]->ID);
+		}elseif(is_single()||is_page()){
+			global $post;
+			
+			return get_permalink($post->ID);
+		}
+	}
+
+	function widget($args, $instance) {
+		extract( $args );
+		
+		$title = apply_filters('widget_title', $instance['title']);
+		
+		$customlink = $instance['customlink'];
+		$permalink = $instance['permalink'];
+		
+		if($customlink)
+			$link = $customlink;
+		else
+			$link = $this->get_post_permalink();
+		
+		$description = $instance['description'];
+		
+		$imagelink = '<input type="hidden" value="'.$link.'"/><img id="sidebar_fb_like" src="'.plugins_url( 'facebook_fan_button.png' , __FILE__ ).'" alt="Facebook Like" />';
+		
+		$link = '<a href="http://www.fanpagecreator.net/facebook-like-datenschutz-button/"><font color="#999999" size="1">Facebook Like Widget</font></font></a>';
+		
+		?>
+		      <?php echo $before_widget; ?>
+		          <?php if ( $title )
+		                echo $before_title . $title . $after_title; ?>
+		        <?php echo $description.$imagelink.$link;?>
+		      <?php echo $after_widget; ?>
+		<?php
+	}
+
+	function update($new_instance, $old_instance) {
+		
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['customlink'] = esc_url_raw($new_instance['customlink']);
+		$instance['permalink'] = ($new_instance['permalink']?true:false);
+		$instance['description'] = $new_instance['description'];
+		
+		return $instance;
+    	}
+
+	function form($instance) {
+		
+		$title = apply_filters('widget_title', $instance['title']);
+		$customlink = $instance['customlink'];
+		$permalink = ($instance['permalink']?true:false);
+		$description = $instance['description'];
+		
+	?>
+		<p>
+		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Titel:'); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		
+		<p>
+		<label for="<?php echo $this->get_field_id('customlink'); ?>"><?php _e('Link (Fanpage URL):'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('customlink'); ?>" name="<?php echo $this->get_field_name('customlink'); ?>" type="text" value="<?php echo $customlink; ?>" />
+		</p>
+		
+		<p>
+		<lable for="<?php echo $this->get_field_id('description'); ?>"><?php _e('Text (HTML ist erlaubt):'); ?></lable>
+		<textarea class="widefat" rows="10" cols="20" id="<?php echo $this->get_field_id('description'); ?>" name="<?php echo $this->get_field_name('description'); ?>" type="textarea"><?php echo $description; ?></textarea>
+		</p>
+		
+	<?php 
+	}
+
+} 
+add_action('widgets_init', create_function('', 'return register_widget("FacebookLike");'));
+
+add_action('wp_enqueue_scripts', 'fblb_include_jquery');
+function fblb_include_jquery(){
+wp_enqueue_script( 'jquery' );
+}
 
 add_action('wp_head','fblb_wp_head');
 function fblb_wp_head(){
-global $post;
-$post_link = urlencode(get_permalink($post->ID));
 $script =<<<EOF
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
 <script type="text/javascript">
 jQuery(document).ready(function(){
-	like_confirm = 'Datenschutz: Bitte erst auf "Ok" klicken um den "Gefällt mir" Button einzublenden und eine Verbindung zu Facebook herzustellen. Dann nochmal drauf drücken.';
-	like_code = '<iframe src="http://www.facebook.com/plugins/like.php?href={$post_link}&amp;layout=standard&amp;show_faces=true&amp;width=450&amp;action=like&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe>';
 
-	jQuery('#fb_like').mouseover(function(){if(confirm(like_confirm)){\$(this).replaceWith(like_code)}});
+	like_confirm = 'Datenschutz: Bitte erst auf "Ok" klicken um den "Gefällt mir" Button einzublenden und eine Verbindung zu Facebook herzustellen. Dann nochmal drauf drücken.';
+	like_code = '<iframe src="http://www.facebook.com/plugins/like.php?href=fblikelink&amp;layout=standard&amp;show_faces=true&amp;width=450&amp;action=like&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe>';
+	like_code_2 = '<iframe src="http://www.facebook.com/plugins/like.php?href=fblikelink&amp;layout=standard&amp;show_faces=true&amp;width=250&amp;action=like&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:250px; height:80px;" allowTransparency="true"></iframe>';
+	jQuery('#sidebar_fb_like').mouseover(function(){if(confirm(like_confirm)){jQuery(this).replaceWith(like_code_2.replace('fblikelink',encodeURIComponent(jQuery(this).prev().val())))}});
+	jQuery('#content_fb_like').mouseover(function(){if(confirm(like_confirm)){jQuery(this).replaceWith(like_code.replace('fblikelink',encodeURIComponent(jQuery(this).prev().val())))}});
 });
 </script>
 EOF;
@@ -43,9 +131,9 @@ function fblb_add_facebook_button( $content ) {
     if(is_single() || is_page()){
             global $post;
             
-            $permalink = urlencode( get_permalink( $post->ID ) );	
+            $permalink = get_permalink( $post->ID );	
             
-            $fb_html = '<img id="fb_like" src="/wp-content/plugins/facebook-like-datenschutz/facebook_like_button.png" alt="Facebook Like" />';
+            $fb_html = '<input type="hidden" value="'.$permalink.'""/><img id="content_fb_like" src="/wp-content/plugins/facebook-like-datenschutz/facebook_like_button.png" alt="Facebook Like" />';
             if ( get_option( 'fblb_add_before') )
                 return $fb_html . $content;
             else
--- readme.txt
+++ readme.txt
@@ -1,24 +1,26 @@
-ï»¿=== Plugin Name ===
+=== Plugin Name ===
 Contributors: fanpagecreator
 Donate link: http://www.fanpagecreator.net/facebook-like-datenschutz-button/
-Tags: facebook button, facebook like, facebook like button, facebook plugin, facebook share button, facebook datenschutz, facebook gefÃ¤llt mir button 
-Requires at least: 2.8 oder hÃ¶her
+Tags: facebook button, facebook like, facebook like button, facebook plugin, facebook share button, facebook datenschutz, facebook gefällt mir button 
+Requires at least: 2.8 oder höher
 Tested up to: 3.0.1
-Stable tag: 1.00
+Stable tag: 1.2
 
-Den deutschen Facebook Like Datenschutz Button datenschutzkonform und rechtssicher einsetzen 
+Den deutsche Facebook Like Datenschutz Button datenschutzkonform und rechtssicher einsetzen 
 
 == Description ==
 
 Warum ein neues Facebook Like Plugin?
 
-Das UnabhÃ¤ngige Landeszentrum fÃ¼r Datenschutz (ULD) fordert alle Webseiten-Betreiber in Schleswig-Holstein auf, ihre Social-Plugins wie auch den â€žGefÃ¤llt mirâ€œ Button (auch Like Button genannt) von ihren Webseiten zu entfernen.
+Das Unabhängige Landeszentrum für Datenschutz (ULD) fordert alle Webseiten-Betreiber in Schleswig-Holstein auf, ihre Social-Plugins wie auch den „Gefällt mir“ Button (auch Like Button genannt) von ihren Webseiten zu entfernen.
 
-Eine Alternative zum â€žnormalenâ€œ Like Button ist eine Variante mit einem Bild und einer Nutzungserlaubnis. Bevor eine Verbindung zu Facebook hergestellt wird, muss dem zugestimmt werden, ansonsten bleibt der Like Button â€žinaktivâ€œ und kann technisch keine Daten Ã¼bermitteln.
+Eine Alternative zum „normalen“ Like Button ist eine Variante mit einem Bild und einer Nutzungserlaubnis. Bevor eine Verbindung zu Facebook hergestellt wird, muss dem zugestimmt werden, ansonsten bleibt der Like Button „inaktiv“ und kann technisch keine Daten übermitteln.
 
 Es sind keine manuellen Anpassungen oder aufwendige Eigenbauten notwendig. Nur wenige Klicks und schon ist der Facebook Like Button datenschutzkonform einsatzbereit.
 
-Erst nach einer NutzungsbestÃ¤tigung und erneuten klicken auf den Button wird der Link zur aktuellen Seite an Facebook gesendet und erscheint im Newsfeed.
+Erst nach einer Nutzungsbestätigung und erneuten klicken auf den Button wird der Link zur aktuellen Seite an Facebook gesendet und erscheint im Newsfeed.
+
+Hinweis: Dieses Plugin ist zudem mit Widgets in der Sidebar als Facecbook Like Box nutzbar.
 
 Mehr Infos und ein Demo-Video gibt es auf http://www.fanpagecreator.net/facebook-like-datenschutz-button/
 
@@ -38,7 +40,7 @@ Alternativ:
 
 Einstellungen:
 
-Sie kÃ¶nnen im Dashboard unter â€žEinstellungenâ€œ >> â€žLesenâ€œ festlegen, ob der Facebook Like Button Ã¼ber oder unter den Artikeln eingeblendet werden soll.
+Sie können im Dashboard unter „Einstellungen“ >> „Lesen“ festlegen, ob der Facebook Like Button über oder unter den Artikeln eingeblendet werden soll. Des Weiteren kann man ein Widget mit dem "Gefällt mir" Feld und einem Link (Fanpage URL) sowie ein Textfeld (HTML fähig) in der Sidebar nutzen. Dazu bewegen Sie das Widget per Drag & Drop an die gewünschte Stelle ihrer Sidebar.
 
 
 == Frequently Asked Questions ==
